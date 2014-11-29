c8981fe980715f3530a8bc23bcf04b41e62bc9b1
Javier Rizzo <javierrizzoa@gmail.com>
2014-11-29 10:00:06 -0800

    Fix Wiimote DLLs

--- WhiteBoard/Pages/Home.xaml.cs
+++ WhiteBoard/Pages/Home.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WiimoteLib;
 
 namespace WhiteBoard.Pages
 {
@@ -20,6 +21,7 @@ namespace WhiteBoard.Pages
     /// </summary>
     public partial class Home : UserControl
     {
+       
         public Home()
         {
             InitializeComponent();
