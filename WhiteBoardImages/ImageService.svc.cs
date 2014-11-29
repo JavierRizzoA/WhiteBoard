8c7361a72da92af926d555d585f7d498bec11575
Macario Hernandez <mackhdez@hotmail.com>
2014-11-29 11:52:09 -0800

    Arquitectura y servicio

---
+++ WhiteBoardImages/ImageService.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhiteBoardImages
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ImageService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ImageService.svc or ImageService.svc.cs at the Solution Explorer and start debugging.
+    public class ImageService : IImageService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
