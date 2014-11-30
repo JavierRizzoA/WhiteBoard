29b5b23b21b2494061e8e94ce3ffa7c60de94971
Macario Hernandez <mackhdez@hotmail.com>
2014-11-29 16:00:53 -0800

    Response result

---
+++ WhiteBoard.Models/Data/ResponseResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WhiteBoard.Models.Data
+{
+    public class ResponseResult<T> : Response
+    {
+
+        public T Data { get; set; }
+
+    }
+}
